54cd596a7c409da2a893dbfb219cf1a2d9def0e9
Evan Chang <toosyou70207@gmail.com>
2016-03-13 01:36:37 +0800

    reindent to 4-space tabs with nothing changed

--- Lab1/alu.v
+++ Lab1/alu.v
@@ -1,4 +1,5 @@
-//0316055
+//0316055_0316313
+
 `timescale 1ns/1ps
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -22,95 +23,97 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module alu(
-           rst_n,         // negative reset            (input)
-           src1,          // 32 bits source 1          (input)
-           src2,          // 32 bits source 2          (input)
-           ALU_control,   // 4 bits ALU control input  (input)
-		 //bonus_control, // 3 bits bonus control input(input) 
-           result,        // 32 bits result            (output)
-           zero,          // 1 bit when the output is 0, zero must be set (output)
-           cout,          // 1 bit carry out           (output)
-           overflow       // 1 bit overflow            (output)
-           );
+    rst_n,         // negative reset            (input)
+    src1,          // 32 bits source 1          (input)
+    src2,          // 32 bits source 2          (input)
+    ALU_control,   // 4 bits ALU control input  (input)
+    //bonus_control, // 3 bits bonus control input(input)
+    result,        // 32 bits result            (output)
+    zero,          // 1 bit when the output is 0, zero must be set (output)
+    cout,          // 1 bit carry out           (output)
+    overflow       // 1 bit overflow            (output)
+    );
 
 
-  input           rst_n;
-  input  [32-1:0] src1;
-  input  [32-1:0] src2;
-  input   [4-1:0] ALU_control;
-  //input   [3-1:0] bonus_control; 
+    input           rst_n;
+    input  [32-1:0] src1;
+    input  [32-1:0] src2;
+    input   [4-1:0] ALU_control;
+    //input   [3-1:0] bonus_control;
 
-  output [32-1:0] result;
-  output          zero;
-  output          cout;
-  output          overflow;
+    output [32-1:0] result;
+    output          zero;
+    output          cout;
+    output          overflow;
 
-  reg    [32-1:0] result;
-  reg             zero;
-  reg             cout;
-  reg             overflow;
+    reg    [32-1:0] result;
+    reg             zero;
+    reg             cout;
+    reg             overflow;
 
-  localparam [3:0] OP_AND=4'b0000, OP_OR=4'b0001, OP_ADD=4'b0010, OP_SUB=4'b0110, OP_NOR=4'b1100, OP_NAND=4'b1101, OP_SLT=4'b0111;
-  reg a, b;
-  reg less;
-  reg a_invert, b_invert;
-  reg 1b_cin;
-  reg [1:0] operation;
-  reg 1b_result, 1b_cout;
-  integer idx;
+    localparam [3:0] OP_AND=4'b0000, OP_OR=4'b0001, OP_ADD=4'b0010, OP_SUB=4'b0110, OP_NOR=4'b1100, OP_NAND=4'b1101, OP_SLT=4'b0111;
+    reg a, b;
+    reg less;
+    reg a_invert, b_invert;
+    reg 1b_cin;
+    reg [1:0] operation;
+    reg 1b_result, 1b_cout;
+    integer idx;
 
-  alu_top 1_bit_alu(
-    .src1(a),
-    .src2(b),
-    .less(less),
-    .A_invert(a_invert),
-    .B_invert(b_invert),
-    .cin(1b_cin),
-    .operation(operation),
-    .result(1b_result),
-    .cout(1b_cout)
-  );
+    alu_top 1_bit_alu(
+        .src1(a),
+        .src2(b),
+        .less(less),
+        .A_invert(a_invert),
+        .B_invert(b_invert),
+        .cin(1b_cin),
+        .operation(operation),
+        .result(1b_result),
+        .cout(1b_cout)
+        );
 
-  always @(posedge clk or posedge rst) begin
-    if (rst) begin
-      case (ALU_control)
-        OP_ADD: begin
-          a_invert = 1'b0;
-          b_invert = 1'b0;
-          operation = 2'b10;
-        end
-        OP_SUB: begin
-          a_invert = 1'b0;
-          b_invert = 1'b1;
-          operation = 2'b10;
-        end
-    end
-  end
-
-  always @(posedge clk or posedge rst) begin
-    if (rst) begin
-      case (ALU_control)
-        OP_ADD: begin
-          for (idx=0; idx<32; idx=idx+1) begin
-            if (idx==0) begin
-              1b_cin = 1'b0;
-            end
-            else begin
-              1b_cin = 1b_cout;
-            end
-          end
-        end
-        OP_SUB: begin
-          for (idx=0; idx<32; idx=idx+1) begin
-            if (idx==0) begin
-              1b_cin = 1'b1;
-            end
-            else begin
-              1b_cin = 1b_cout;
-            end
-          end
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            case (ALU_control)
+                OP_ADD: begin
+                    a_invert = 1'b0;
+                    b_invert = 1'b0;
+                    operation = 2'b10;
+                end
+                OP_SUB: begin
+                    a_invert = 1'b0;
+                    b_invert = 1'b1;
+                    operation = 2'b10;
+                end
+            endcase
         end
     end
-  end
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            case (ALU_control)
+                OP_ADD: begin
+                    for (idx=0; idx<32; idx=idx+1) begin
+                        if (idx==0) begin
+                            1b_cin = 1'b0;
+                        end
+                        else begin
+                            1b_cin = 1b_cout;
+                        end
+                    end
+                end
+                OP_SUB: begin
+                    for (idx=0; idx<32; idx=idx+1) begin
+                        if (idx==0) begin
+                            1b_cin = 1'b1;
+                        end
+                        else begin
+                            1b_cin = 1b_cout;
+                        end
+                    end
+                end
+            endcase
+        end
+    end
 
 endmodule
--- Lab1/alu_top.v
+++ Lab1/alu_top.v
@@ -2,72 +2,71 @@
 `timescale 1ns/1ps
 
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date:    10:58:01 02/25/2016
-// Design Name: 
-// Module Name:    alu_top 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Design Name:
+// Module Name:    alu_top
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Dependencies: 
+// Dependencies:
 //
-// Revision: 
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 
 module alu_top(
-               src1,       //1 bit source 1 (input)
-               src2,       //1 bit source 2 (input)
-               less,       //1 bit less     (input)
-               A_invert,   //1 bit A_invert (input)
-               B_invert,   //1 bit B_invert (input)
-               cin,        //1 bit carry in (input)
-               operation,  //operation      (input)
-               result,     //1 bit result   (output)
-               cout       //1 bit carry out(output)
-               );
+    src1,       //1 bit source 1 (input)
+    src2,       //1 bit source 2 (input)
+    less,       //1 bit less     (input)
+    A_invert,   //1 bit A_invert (input)
+    B_invert,   //1 bit B_invert (input)
+    cin,        //1 bit carry in (input)
+    operation,  //operation      (input)
+    result,     //1 bit result   (output)
+    cout       //1 bit carry out(output)
+    );
 
-     input src1;
-     input src2;
-     input less;
-     input A_invert;
-     input B_invert;
-     input cin;
-     input [2-1:0] operation;
+    input src1;
+    input src2;
+    input less;
+    input A_invert;
+    input B_invert;
+    input cin;
+    input [2-1:0] operation;
 
-     output result;
-     output cout;
+    output result;
+    output cout;
 
-     reg    result;
-     reg    cout;
+    reg    result;
+    reg    cout;
 
-     localparam [1:0] OP_AND = 2'b00, OP_OR = 2'b01, OP_ADD = 2'b10;
-     reg a, b;
-     reg g, p;
+    localparam [1:0] OP_AND = 2'b00, OP_OR = 2'b01, OP_ADD = 2'b10;
+    reg a, b;
+    reg g, p;
 
-     assign a = A_invert? !src1: src1;
-     assign b = B_invert? !src2: src2;
-     assign g = a & b;
-     assign p = a | b;
+    assign a = A_invert? !src1: src1;
+    assign b = B_invert? !src2: src2;
+    assign g = a & b;
+    assign p = a | b;
 
-     always@( * )
-     begin
-          if (operation==OP_AND) begin
-               result <= g;
-          end
-          else if (operation==OP_OR) begin
-               result <= p;
-          end
-          else begin//operation==OP_ADD
-               result <= a ^ b ^ cin;
-               cout <= g | (p&cin);
-          end
-     end
+    always@( * )begin
+        if (operation==OP_AND) begin
+            result <= g;
+        end
+        else if (operation==OP_OR) begin
+            result <= p;
+        end
+        else begin//operation==OP_ADD
+            result <= a ^ b ^ cin;
+            cout <= g | (p&cin);
+        end
+    end
 
 endmodule
