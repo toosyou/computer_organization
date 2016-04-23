f3b493df1f7dbcc9f8d8e7674978583176aa14aa
toosyou <toosyou70207@gmail.com>
2016-04-24 03:31:44 +0800

    alu ctrl done without test

--- Lab2/code/ALU_Ctrl.v
+++ Lab2/code/ALU_Ctrl.v
@@ -26,10 +26,37 @@ module ALU_Ctrl(
 	reg     [4-1:0] ALUCtrl_o;
 
 	//Parameter
-
+	parameter CTRL_ADD = 4'b0010;
+	parameter CTRL_SUB = 4'b0110;
+	parameter CTRL_AND = 4'b0000;
+	parameter CTRL_OR  = 4'b0001;
+	parameter CTRL_SLT = 4'b0111;
 	       
 	//Select exact operation
-	
+	always@(*)begin
+		case( ALUOp_i )
+			case 0:begin
+				case( funct_i )
+					// add r1,r2,r3
+					case 32:ALUCtrl_o = CTRL_ADD;
+					// sub r1,r2,r3
+					case 34:ALUCtrl_o = CTRL_SUB;
+					// and r1,r2,r3
+					case 36:ALUCtrl_o = CTRL_AND;
+					// or r1,r2,r3
+					case 37:ALUCtrl_o = CTRL_OR;
+					// slt r1,r2,r3
+					case 42:ALUCtrl_o = CTRL_SLT;
+				endcase
+			end
+			//addi r1,r2,100
+			case 8:ALUCtrl_o = CTRL_ADD;
+			//sltiu r1,r2,10
+			case 9:ALUCtrl_o = CTRL_SLT;
+			//beq r1,r2,25
+			case 4:ALUCtrl_o = CTRL_SUB;
+		endcase
+	end
 
 endmodule     
 
