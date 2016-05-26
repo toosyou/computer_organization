6f8d4132a69e4d71311d84c42c78a23673d73184
toosyou <toosyou70207@gmail.com>
2016-05-26 14:14:36 +0800

    basic-IF stage done without test

--- Lab4/code/Pipe_CPU_1.v
+++ Lab4/code/Pipe_CPU_1.v
@@ -23,7 +23,10 @@ input rst_i;
 Internal signal
 ****************************************/
 /**** IF stage ****/
-
+	wire [31:0] IF_pc_mux_out;
+	wire [31:0] IF_pc_out;
+	wire [31:0] IF_pc_add4;
+	wire [31:0] IF_instruction;
 
 /**** ID stage ****/
 
@@ -36,6 +39,8 @@ Internal signal
 
 
 /**** MEM stage ****/
+	wire MEM_PCSrc;
+	wire [31:0] MEM_ALU_result;
 
 //control signal
 
@@ -49,21 +54,37 @@ Internal signal
 Instnatiate modules
 ****************************************/
 //Instantiate the components in IF stage
-ProgramCounter PC(
+MUX_2to1 #(.size(32)) PC_MUX(
+		.data0_i(IF_pc_add4),
+		.data1_i(MEM_ALU_result),
+		.select_i(MEM_PCSrc),
+		.data_o(IF_pc_mux_out)
+		);
 
+ProgramCounter PC(
+		.clk_i(clk_i),
+		.rst_i(rst_i),
+		.pc_in_i(IF_pc_mux_out),
+		.pc_out_o(IF_pc_out)
         );
 
 Instr_Memory IM(
-
+		.addr_i(IF_pc_out),
+		.instr_o(IF_instruction)
 	    );
 			
 Adder Add_pc(
-
+		.src1_i(IF_pc_out),
+		.src2_i(32'd4),
+		.sum_o(IF_pc_add4)
 		);
 
 		
-Pipe_Reg #(.size(N)) IF_ID(       //N is the total length of input/output
-
+Pipe_Reg #(.size(32*2)) IF_ID(//top to down
+		.clk_i(clk_i),
+		.rst_i(rst_i),
+		.data_i({IF_pc_add4,IF_instruction}),
+		.data_o()
 		);
 		
 //Instantiate the components in ID stage
